5f269fb6a71d9b782d6662f71c31e31d8aaea204
kodfactory <52972405+kodfactory@users.noreply.github.com>
2020-12-04 21:24:31 +0530

    Add files via upload
    First program

---
+++ FirstProg.py
@@ -0,0 +1,38 @@
+# Sep in print
+# Variable naming convention
+
+#a = 5
+#b = 10
+#c = 20
+#print(a,b,c, sep='&')
+
+# Variable name should be readable (meaningful)
+
+#name1 = 'Ronak'
+#name2 = 'Miyavara'
+
+#firstname = 'Ronak'
+#lastname = 'Miyavara'
+
+#firstnameandlastname = 'Jayesh Dhandha'
+
+# Camle case naming convention
+# First character of every word should be capital except 1st word
+
+#firstNameAndLastName = 'Jayesh Dhandha'
+#first_name_and_last_name = 'Jayesh Dhandha'
+
+# Variables Naming Rules:
+# 1. There should not be any space in between words in variable
+# 2. It should readable
+# 3. It should not be fully numeric
+# 4. It should not contain like special character ($#%&)
+# 5. It should not start with number (It can start with alphabet or _)
+
+_name = 'Jayesh'
+
+print(_name)
+
+
+
+
